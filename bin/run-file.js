ab2d9ef44afd6b9277145b4143f5ce2ed72e5f10
saurabh <saurabhmehta1601@gmail.com>
2021-09-08 15:52:01 +0530

    support .js file execution

--- bin/run-file.js
+++ bin/run-file.js
@@ -26,5 +26,16 @@ if(ext == "cpp"){
             log(chalk.yellow.bgRed.bold("File compilation unsuccessfull.") + "⚠️")
         }
     })
+}else if(ext === "js"){
+    const interpret = spawn('node',[file],{stdio: [process.stdin, 'pipe', 'pipe']})
+    interpret.stdout.on('data', data =>{
+        log(chalk.blue.bold(data.toString()))
+    })
+    interpret.on('close',(code) =>{
+        if(code === 0 ){
+            log(chalk.black.bgBlue.bold("Program Successully terminated") + "✅")
+        }else{
+            log(chalk.yellow.bgRed.bold("File interpretion unsuccessfull.") + "⚠️")
+        }
+    })
 }
-
