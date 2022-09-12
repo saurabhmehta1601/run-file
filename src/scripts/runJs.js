9a65c92b788103d002c07a01a1ca958e8d6a422b
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-09-12 13:57:21 +0530

    fix: check is nodejs is installed before running js files

--- src/scripts/runJs.js
+++ src/scripts/runJs.js
@@ -1,20 +1,30 @@
 const { spawn } = require("child_process");
+const hasbin = require("hasbin");
 const { logConsole, logWarn, logSuccess, logError } = require("../utils/log");
 
 module.exports = (file) => {
-  const interpret = spawn("node", [file], {
-    stdio: [process.stdin, "pipe", "pipe"],
-  });
-  interpret.stdout.on("data", (data) => {
-    logConsole(data.toString());
-  });
-  interpret.stderr.on("data", (error) => {
-    logError(error);
+  if (hasbin.sync("node")) {
+    const interpret = spawn("node", [file], {
+      stdio: [process.stdin, "pipe", "pipe"],
+    });
+    interpret.stdout.on("data", (data) => {
+      process.stdout.write(data.toString());
+    });
+    interpret.stderr.on("data", (error) => {
+      logError(error);
+      process.exit(1);
+    });
+    interpret.on("close", (code) => {
+      console.log("\n");
+      if (code === 0) {
+        logSuccess("Program terminated .");
+      }
+      process.exit(code);
+    });
+  } else {
+    logError(
+      "Please install nodejs and check if it is installed with command node --version"
+    );
     process.exit(1);
-  });
-  interpret.on("close", (code) => {
-    if (code === 0) {
-      logSuccess("Program terminated .");
-    }
-  });
+  }
 };
